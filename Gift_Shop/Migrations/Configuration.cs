bf4dbf66d4bdd83d86eddbb2bfa53e35a433e4da
Rene Gonzalez Cota <rgonzalez@pinnacleaerospace.com>
2018-05-22 17:23:38 -0700

    Added apicontroller

--- Gift_Shop/Migrations/Configuration.cs
+++ Gift_Shop/Migrations/Configuration.cs
@@ -16,18 +16,6 @@ namespace Gift_Shop.Migrations
 
         protected override void Seed(Gift_Shop.DAL.GiftShopContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
             var roles = new List<Role>
             {
                 new Role { id=1, role="Admin"},
