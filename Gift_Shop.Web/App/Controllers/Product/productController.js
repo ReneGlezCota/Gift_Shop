0b6e8d3f1fac757f2bbbfca11ab1d25184ebe270
ReneGlezCota <reneglezcota@gmail.com>
2018-06-03 22:55:49 -0600

    Added new templates

--- Gift_Shop.Web/App/Controllers/Product/productController.js
+++ Gift_Shop.Web/App/Controllers/Product/productController.js
@@ -17,19 +17,92 @@ angular
             if ($scope.productname != '') {
                 var obj = _($scope.products).filter(
                     function (r) {
-                        for (var key in r) {
-                            if (_(r[key]).toString().toUpperCase().includes($scope.productname.toString().toUpperCase())) { return true; };
-                        }
+                        if (_(r['Name']).toString().toUpperCase().includes($scope.productname.toString().toUpperCase())) { return true; };                        
                         return false;
                     }
                 ).value();
 
                 $scope.filteredItems = obj;
             };
-
-            console.log($scope.filteredItems);
-
         }
-        
 
-    }]);
+        $scope.updateItem = function (value) {
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'App/Views/Modals/productUpdate.html',
+                controller: 'ModalUpdateProductController',
+                resolve: {
+                    details: function () {
+                        return value;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (values) {
+                initProduct();
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });       
+        };
+
+        $scope.deleteItem = function (value) {        
+            var modalInstance = $uibModal.open({
+                animation: true,
+                templateUrl: 'App/Views/Modals/productDelete.html',
+                controller: 'ModalDeleteProductController',
+                resolve: {
+                    details: function () {
+                        return value;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (values) {
+                initProduct();
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });       
+        };
+    }])
+    .controller('ModalUpdateProductController', function ($scope, $uibModalInstance, details, ProductService, CategoryService) {
+        console.log(details);
+        $scope.details = details;
+        $scope.promiseUpdate = '';
+        $scope.categories = '';
+
+        var initCategories = function () {
+            $scope.promiseCategory = CategoryService.getAllCategories().then(function (result) {
+                $scope.categories = result.data;
+                if ($scope.categories != null) {
+                    $scope.category = details.CategoryID;
+                }
+            });
+        };
+
+        initCategories();
+
+        $scope.update = function () {
+            //$scope.promiseDelete = ProductService.updateProduct($scope.details.ProductID).then(function (result) {
+            //    $uibModalInstance.close(result.data);
+            //});
+        };
+
+        $scope.closeUpdate = function () {
+            $uibModalInstance.close();
+        };
+    })
+    .controller('ModalDeleteProductController', function ($scope, $uibModalInstance, details, ProductService) {
+        console.log(details);
+        $scope.details = details;
+        $scope.promiseDelete = '';
+
+        $scope.deleteProduct = function () {
+            //$scope.promiseDelete = ProductService.deleteProduct($scope.details.ProductID).then(function (result) {
+            //    $uibModalInstance.close(result.data);
+            //});
+        };  
+
+        $scope.closeDetails = function () {
+            $uibModalInstance.close();
+        };
+    });
--- Gift_Shop.Web/App/Services/ProductService.js
+++ Gift_Shop.Web/App/Services/ProductService.js
@@ -12,6 +12,28 @@ angular
                     console.log('Error in the charge of values ' + err);
                 });
                 return data;
+            },
+            deleteProduct: function (productID) {
+                var data = $http.delete('/api/product/', productID).then(function (result) {
+                    return result;
+                }, function (err) {
+                    console.log('Error in the charge of values ' + err);
+                });
+                return data;
+            },
+            updateProduct: function (values) {
+                var values = JSON.stringify({
+                    productID: values.ProductID,
+                    description: values.Description,
+                    price: values.Price,
+                    name: values.Name
+                });
+                var data = $http.post('/api/product/', values).then(function (result) {
+                    return result;
+                }, function (err) {
+                    console.log('Error in the charge of values ' + err);
+                });
+                return data;
             }
         };
         return GetApiProducts;
